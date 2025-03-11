2f7c5bd7ff44835969029b1bb490a39b8e88bb8d
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-11 12:03:48 -0500

    Genera un ńumero aleatorio

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+
+let numeroSecreto = generarNumeroSecreto();
+
+console.log(numeroSecreto);
+
 function asignarTextoElemento(elemento , texto){
     let titulo = document.querySelector(elemento);
-    titulo.innerHTML= texto;    
+    titulo.innerHTML= texto;
+    return;
 }
 function intentoDeUsuario(){
     alert ('Click desde la funcion');
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
 }
 
 asignarTextoElemento('h1','Juego Del Número Secreto');
