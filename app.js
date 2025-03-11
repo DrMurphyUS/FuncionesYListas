7c7c8c5d49a8a2c793d1964cc2e469bf14a18cc1
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-11 11:40:16 -0500

    Asigna texto a un elemento HTML

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML= 'Juego del Número Secreto'
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un numero del 1 al 10';
+function asignarTextoElemento(elemento , texto){
+    let titulo = document.querySelector(elemento);
+    titulo.innerHTML= texto;    
+}
 function intentoDeUsuario(){
     alert ('Click desde la funcion');
 }
+
+asignarTextoElemento('h1','Juego Del Número Secreto');
+asignarTextoElemento('p','Indica un numero del 1 al 100');
