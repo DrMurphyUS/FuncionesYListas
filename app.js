83fdea245a28d770c80a2804dcd622c407d14692
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-11 10:59:24 -0500

    Evento click y primera funcion

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ let titulo = document.querySelector('h1');
 titulo.innerHTML= 'Juego del Número Secreto'
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un numero del 1 al 10';
+function intentoDeUsuario(){
+    alert ('Click desde la funcion');
+}
