ac868a66c5ade3fe1f6b6a8566f05283253581bb
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-11 10:23:29 -0500

    Añadiendo texto desde JS

---
+++ app.js
@@ -0,0 +1,4 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML= 'Juego del Número Secreto'
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10';
